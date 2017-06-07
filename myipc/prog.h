109f6afbcf7bf5068138afd6b5309ebc3756cd34
Chris <ic16b055@technikum-wien.at>
2017-06-07 21:04:04 +0200

    new

---
+++ myipc/prog.h
@@ -0,0 +1,47 @@
+/**
+*
+* @file prog.h
+* Betriebssysteme myipc
+* Beispiel 3
+*
+* @author Maria Kanikova <ic16b002@technikum-wien.at>
+* @author Christian Fuhry <ic16b055@technikum-wien.at>
+* @author Sebastian Boehm <ic16b032@technikum-wien.at>
+*
+* @date 2017/06
+*
+*/
+
+#ifndef MYIPC_H
+#define MYIPC_H
+
+/*
+ * -------------------------------------------------------------- includes --
+ */
+
+#include <stdio.h>
+
+/*
+ * --------------------------------------------------------------- defines --
+ */
+
+/*
+ * -------------------------------------------------------------- typedefs --
+ */
+
+/*
+ * --------------------------------------------------------------- globals --
+ */
+
+/*
+ * ------------------------------------------------------------- functions --
+ */
+
+
+
+
+#endif		//MYIPC_H
+
+/*
+ * ------------------------------------------------------------------- eof --
+ */
