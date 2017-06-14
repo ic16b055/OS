a8abf48dc72f46299c61e4f10d5f33056a778738
Chris <ic16b055@technikum-wien.at>
2017-06-14 15:55:10 +0200

    new

--- myipc/prog.c
+++ myipc/prog.c
@@ -44,20 +44,21 @@
 /*
 * --------------------------------------------------------------- globals --
 */
+//Ringbuffer Pointer
+static int* shmptr = (int*)-1;
+//Ringbuffer Size
+static long smsize = 0;
 
-static int* ringbuffer_id = (int*)-1;
-static long ringbuffer_size = 0;
+static int sem1id = -1;
+static int sem2id = -1;
 
-static int semread_id = -1;
-static int semwrite_id = -1;
-
-static int sharedmem_id = -1;
+static int shmid = -1;
 
 /*
 * ------------------------------------------------------------- functions --
 */
 
-int check_parms(const int argc, char* argv[]);				//OK
+int check_parms(const int argc, char* argv[]);				
 
 int init_semaphore(const int key, const int mode);
 int block(const int mode);
@@ -80,117 +81,117 @@ int cleanup(const int mode);
 *
 */
 int run(const int argc, char* argv[], const int mode) {
-	
+
 	int temp = 0;
 	int counter = 0;
-	
-	int userid = getuid() * 1000 + 14;	
-	
-	check_parms(argc,argv);			
-	
-	semread_id = init_semaphore(userid, EMPFAENGER);
-	semwrite_id = init_semaphore(userid + 1, SENDER);
+
+	int userid = getuid() * 1000 + 14;
+
+	check_parms(argc, argv);
+
+	sem1id = init_semaphore(userid, EMPFAENGER);
+	sem2id = init_semaphore(userid + 1, SENDER);
 	init_sharedmem(userid + 2);							//vielleicht nur bei Sender?
 
-	
+
 	if (mode == SENDER) {
-		
-		ringbuffer_id = shmat(sharedmem_id, NULL, 0);		//das erzeugte Shared-Memory-Segment an einen Prozess anbinden
-				
+
+		shmptr = shmat(shmid, NULL, 0);		//das erzeugte Shared-Memory-Segment an einen Prozess anbinden
+
 		do {
 			temp = fgetc(stdin);
 			if (ferror(stdin)) {
-				errorhandling(mode, "Cannot read fgetc");		
+				errorhandling(mode, "Cannot read fgetc");
 			}
 
 
 			if (block(mode) == -1) {
 				errorhandling(mode, "Semaphore block error");
 			}
-			ringbuffer_id[counter] = temp;					// critical section 
-			
+			shmptr[counter] = temp;					// critical section 
+
 			if (unblock(mode) == -1) {
 				errorhandling(mode, "Semaphore unblock error");
 			}
-			
-		counter = (counter + 1) % ringbuffer_size;
+
+			counter = (counter + 1) % smsize;
 
 		} while (temp != EOF);
 	}
 	else {
-		
-		ringbuffer_id = shmat(sharedmem_id, NULL, SHM_RDONLY);   //das erzeugte Shared-Memory-Segment an einen Prozess anbinden
-		
-		
+
+		shmptr = shmat(shmid, NULL, SHM_RDONLY);   //das erzeugte Shared-Memory-Segment an einen Prozess anbinden
+
+
 		do {
-			
+
 			if (block(mode) == -1) {
 				errorhandling(mode, "Semaphore block error");
 			}
-			temp = ringbuffer_id[counter];					// critical section  
-			
+			temp = shmptr[counter];					// critical section  
+
 			if (unblock(mode) == -1) {
 				errorhandling(mode, "Semaphore unblock error");
 			}
 
-			if (temp != EOF) 
+			if (temp != EOF)
 
-			if	(fputc(temp, stdout) < 0) {
-				errorhandling(mode, "Cannot write fputc");
-			}
-				
-					counter = (counter + 1) % ringbuffer_size;
+				if (fputc(temp, stdout) < 0) {
+					errorhandling(mode, "Cannot write fputc");
+				}
 
-				} while (temp != EOF);
+			counter = (counter + 1) % smsize;
 
-			}
+		} while (temp != EOF);
 
-	cleanup(mode);	
-		
+	}
+
+	cleanup(mode);
+
 	return 0;
 }
 
 /**
 *
-* \name 
+* \name
 *
-* \brief 
-* 
-* \param 	
+* \brief
 *
-* \return 	
+* \param
+*
+* \return
 *
 */
-int check_parms(const int argc, char* argv[]) {												
-	ringbuffer_size = 0;
+int check_parms(const int argc, char* argv[]) {
+	smsize = 0;
 	int usage = 0;
 	int option = 0;
 	char* endptr = NULL;
 	errno = 0;
-	
+
 	if (argc < 2) {
-	usage = 1;
+		usage = 1;
 	}
-	
-	while ((option = getopt(argc, argv, "m:")) != -1) {	
-		
+
+	while ((option = getopt(argc, argv, "m:")) != -1) {
+
 		errno = 0;
 		if (optind < argc) {
 			fprintf(stderr, "Usage: -m Too many arguments\n");
 			exit(EXIT_FAILURE);
 		}
 		if (option == 'm') {
-			ringbuffer_size = strtol(optarg, &endptr, 10);
+			smsize = strtol(optarg, &endptr, 10);
 
 			if (*endptr != '\0') {
 				fprintf(stderr, "Usage: -m Characters found in buffersize\n");
 				exit(EXIT_FAILURE);
-			}		
-			if (errno == ERANGE && ringbuffer_size == LONG_MAX) {
-				fprintf(stderr, "Usage: -m Buffersize to large\n");
-				exit(EXIT_FAILURE);						
 			}
-			if (ringbuffer_size <= 0) {
+			if (errno == ERANGE && smsize == LONG_MAX) {
+				fprintf(stderr, "Usage: -m Buffersize to large\n");
+				exit(EXIT_FAILURE);
+			}
+			if (smsize <= 0) {
 				fprintf(stderr, "Usage: -m Buffersize to small\n");
 				exit(EXIT_FAILURE);
 			}
@@ -223,20 +224,20 @@ int init_semaphore(const int key, const int mode) {
 	errno = 0;
 
 	if (mode == SENDER) {					//buffer für sender oder init buffer 0 für empfänger
-		init_buffer = ringbuffer_size;
+		init_buffer = smsize;
 	}
 
 	semid = seminit(key, 0660, init_buffer);
-	
+
 	if (errno == EEXIST) {
 		semid = semgrab(key);
 	}
-	
+
 	if (semid == -1) {
 		fprintf(stderr, "Cannot grab semaphore\n");
 		exit(EXIT_FAILURE);
 	}
-																
+
 	return semid;
 }
 
@@ -254,10 +255,10 @@ int init_semaphore(const int key, const int mode) {
 
 int block(const int mode) {
 
-	int sem_id = semwrite_id;
+	int sem_id = sem2id;
 
 	if (mode == EMPFAENGER) {
-		sem_id = semread_id;
+		sem_id = sem1id;
 	}
 
 	if (P(sem_id) == -1) {
@@ -279,10 +280,10 @@ int block(const int mode) {
 */
 int unblock(const int mode) {
 
-	int sem_id = semread_id;
+	int sem_id = sem1id;
 
 	if (mode == EMPFAENGER) {
-		sem_id = semwrite_id;
+		sem_id = sem2id;
 	}
 
 	if (V(sem_id) == -1) {
@@ -304,7 +305,7 @@ int unblock(const int mode) {
 */
 int init_sharedmem(const int key) {
 
-	sharedmem_id = shmget(key, sizeof(int) * ringbuffer_size, 0666 | IPC_CREAT);
+	shmid = shmget(key, sizeof(int) * smsize, 0660 | IPC_CREAT);
 
 	return 0;
 }
@@ -325,10 +326,10 @@ void errorhandling(const int mode, const char *error_message) {
 	printf("ERROR %c \n", *error_message);
 
 
-	
+
 	cleanup(mode);
 
-exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
 
@@ -344,20 +345,20 @@ exit(EXIT_FAILURE);
 *
 */
 int cleanup(const int mode) {
-	
-	shmdt(ringbuffer_id);		//Anbindung Shared-Memory-Segment vom Prozess aufheben
-	
+
+	shmdt(shmptr);		//Anbindung Shared-Memory-Segment vom Prozess aufheben
+
 	if (mode == EMPFAENGER) {
 
-	semrm(semread_id);
-	semread_id = -1;
+		semrm(sem1id);
+		sem1id = -1;
 
-	semrm(semwrite_id);
-	semwrite_id = -1;
+		semrm(sem2id);
+		sem2id = -1;
 
-	shmctl(sharedmem_id, IPC_RMID, NULL);
-	sharedmem_id = -1;
-	
+		shmctl(shmid, IPC_RMID, NULL);
+		shmid = -1;
+
 	}
 	if (fflush(stdout) == EOF) {
 		fprintf(stderr, "Error flushing stdout");
